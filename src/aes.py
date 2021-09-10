cc97d8dd3d0a915eee33b75fcd8e3cc9ccda8aef
JD297 <jan.dommasch297@gmail.com>
2021-09-10 19:37:34 +0200

    Modified AES wrapper

--- src/aes.py
+++ src/aes.py
@@ -13,40 +13,51 @@ from Crypto.Hash import SHA256
 
 chunks = 32 * 1024
 
-# todo
-# encrypt input into session-file (maybe an encrypted json file)
-def encrypt(key, filename):
-	out_file_name = "encrypted-" + os.path.basename(filename)
-	file_size = str(os.path.getsize(filename)).zfill(16) # 0000000000032001
-	IV = Random.new().read(16)
-	encryptor = AES.new(key, AES.MODE_CFB, IV)
-	with open(filename, 'rb') as f_input:
-		with open(out_file_name, 'wb') as f_output:
-			f_output.write(file_size.encode('utf-8'))
-			f_output.write(IV)
-			while True:
-				chunk = f_input.read(chunks)
-				if len(chunk) == 0:
-					break
-				if len(chunk) % 16 != 0:
-					chunk += b' ' * (16 - (len(chunk) % 16))
-				f_output.write(encryptor.encrypt(chunk))
+def encrypt(file_path, password, data):
+	key = get_key(password)
 
-# todo
-# decrypt session-file into variable (from json to: list or map)
-def decrypt(key, filename):
-	out_file_name = filename.split("-")[-1]
-	with open(filename, 'rb') as f_input:
+	file_size = str(len(data)).zfill(16)
+
+	IV = Random.new().read(16)
+	encryptor = AES.new(key, AES.MODE_CBC, IV)
+
+	with open(file_path, 'wb') as f_output:
+		f_output.write(file_size.encode('utf-8'))
+		f_output.write(IV)
+
+		chunk_pos = 0
+
+		while True:
+			chunk = bytes(data[chunk_pos:chunks], "utf-8")
+
+			if len(chunk) == 0:
+				break
+
+			if len(chunk) % 16 != 0:
+				chunk += b' ' * (16 - (len(chunk) % 16))
+				chunk_pos += chunks
+
+			f_output.write(encryptor.encrypt(chunk))
+
+def decrypt(file_path, password):
+	key = get_key(password)
+
+	data = ""
+
+	with open(file_path, 'rb') as f_input:
 		filesize = int(f_input.read(16))
 		IV = f_input.read(16)
-		decryptor = AES.new(key, AES.MODE_CFB, IV)
-		with open(out_file_name, 'wb') as f_output:
-			while True:
-				chunk = f_input.read(chunks)
-				if len(chunk) == 0:
-					break
-				f_output.write(decryptor.decrypt(chunk))
-				f_output.truncate(filesize)
+		decryptor = AES.new(key, AES.MODE_CBC, IV)
+
+		while True:
+			chunk = f_input.read(chunks)
+
+			if len(chunk) == 0:
+				break
+
+			data += decryptor.decrypt(chunk).decode("utf-8")[:filesize]
+
+		return data
 
 def get_key(password):
 	hashing = SHA256.new(password.encode('utf-8'))
